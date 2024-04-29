d124cacbfb7406380bcbd74a0cd1b70cfae79e7a
Henrik <henrrb@stud.ntnu.no>
2024-04-29 02:05:14 +0200

    Update settings

--- src/mis/settings.py
+++ src/mis/settings.py
@@ -1,8 +1,9 @@
 import torch
 from pathlib import Path
 
-BRATS_PATH = Path("/Users/eiriksteen/Personal/school/datasyn/TDT4265-Project-Track-2/data/brats2020")
-ASOCA_PATH = Path.cwd() / "ASOCA"
+BRATS_PATH = Path("C:/Users/henri/Desktop/NTNU/4.Året/Vår/TDT 4265 - Computer Vision/TDT4265-Computer-Vision/TDT4265-Project-Track-2/data/brats2020")
+# ASOCA_PATH = Path.cwd() / "ASOCA"
+ASOCA_PATH = Path("C:/Users/henri/Desktop/NTNU/4.Året/Vår/TDT 4265 - Computer Vision/TDT4265-Computer-Vision/TDT4265-Project-Track-2/data/asoca")     # Fix this
 
 DEVICE = (
     "mps"
