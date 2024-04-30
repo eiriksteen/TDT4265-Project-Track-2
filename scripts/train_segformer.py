a7400ad9cfc8c73d404b75b12542161e465bee19
Henrik <henrrb@stud.ntnu.no>
2024-04-30 02:54:24 +0200

    Print images

--- scripts/train_segformer.py
+++ scripts/train_segformer.py
@@ -1,4 +1,6 @@
+import json
 import torch
+import time
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,18 +17,14 @@ from pprint import pprint
 from mis.datasets import ASOCADataset
 from mis.settings import DEVICE, ASOCA_PATH
 from mis.loss import dice_loss, gdlv_loss, focal_loss
+import argparse
+import seaborn as sns
 
-# def dice_loss(y_true: torch.Tensor, y_pred: torch.Tensor):
-#     n = len(y_true.flatten())
-#     return (1/n) * (torch.sum(1 - (2*y_true*y_pred + 1)/(y_true+y_pred + 1)))
+sns.set_style("darkgrid")
+plt.rc("figure", figsize=(16, 8))
+plt.rc("font", size=13)
 
-# def focal_loss(y_true: torch.Tensor, y_pred: torch.Tensor, gamma: float = 2.0):
-
-#     logpt = F.binary_cross_entropy(y_pred, y_true)
-#     pt = torch.e ** (-logpt)
-
-#     return torch.mean((1 - pt)**gamma * logpt)
-
+torch.manual_seed(0)
 
 def train_segformer(
         segformer,
@@ -44,6 +42,7 @@ def train_segformer(
     optimizer = torch.optim.AdamW(segformer.parameters(), lr=lr)
     min_val_loss = float("inf")
     out_dir.mkdir(exist_ok=True)
+    metrics = []
     train_losses, val_losses = [], []
 
     for epoch in range(num_epochs):
@@ -55,7 +54,7 @@ def train_segformer(
         pbar = tqdm(train_loader)
         for i, batch in enumerate(pbar):
 
-            if i < 4:
+            if i < 2:
 
                 # Prepare data
                 image = batch["image"]
@@ -97,8 +96,10 @@ def train_segformer(
 
         segformer.eval()
         val_loss = 0
+        total_imgs, total_preds, total_segs = [], [], []
         with torch.no_grad():
-            for i, batch in enumerate(tqdm(validation_loader)):
+            pbar = tqdm(validation_loader)
+            for i, batch in enumerate(pbar):
                 
                 if i < 2:
                     
@@ -128,29 +129,74 @@ def train_segformer(
                     loss = loss_fn(masks_fi, predicted_masks_fi)
                     loss = Variable(loss, requires_grad=True)
                     val_loss += loss.item()
+                    
+                    # Store images, predictions and masks
+                    # preds = torch.where(logits >= 0.5, 1.0, 0.0)
+                    total_imgs += image.detach().cpu().tolist()
+                    total_preds += predicted_masks[:, None, :, :].detach().cpu().tolist()
+                    total_segs += seg[:, None, :, :].detach().cpu().tolist()
+                
+                    if i % 2 == 0:
+                        pbar.set_description(f"Validation loss at step {i} = {train_loss / (i+1)}")
                 
                 else:
                     break
 
-        metrics = {
-            "train_loss": train_loss / len(train_loader),
-            "val_loss": val_loss / len(validation_loader)
-        }
+        total_segs_np = np.asarray(total_segs)
+        total_preds_np = np.asarray(total_preds)
+        a = accuracy_score(total_segs_np.flatten(), total_preds_np.flatten())
+        p, r, f, s = precision_recall_fscore_support(total_segs_np.flatten(), total_preds_np.flatten())
 
-        if metrics["val_loss"] < min_val_loss:
-            print("New min loss, saving model...")
-            min_val_loss = metrics["val_loss"]
-            torch.save(segformer.state_dict(), out_dir / "segformer")
+        train_loss = train_loss / len(train_loader)
+        val_loss = val_loss / len(validation_loader)
 
+        metrics.append({
+            "val_loss": val_loss,
+            "train_loss": train_loss,
+            "accuracy": a,
+            "precision": p.tolist(),
+            "recall": r.tolist(),
+            "f1": f.tolist(),
+            "support": s.tolist()
+        })
+        
         train_losses.append(train_loss)
         val_losses.append(val_loss)
 
-        pprint(metrics)
+        if val_loss < min_val_loss:
+            print("New min loss, saving model...")
+            torch.save(segformer.state_dict(), out_dir / "segformer")
+            min_val_loss = val_loss
+            
+            epoch_dir = out_dir / f"epoch{epoch+1}"
+            epoch_dir.mkdir(exist_ok=True)
+  
+            with open(epoch_dir / "metrics.json", "w") as f:
+                json.dump(metrics[-1], f, indent=4)
+
+            total_imgs_np = np.asarray(total_imgs)
+            rand_idx = np.random.choice(len(total_preds), 2, replace=False)
+
+            for idx in rand_idx:
+                _, ax = plt.subplots(ncols=3)
+                img = total_imgs_np[idx]
+                seg = total_segs_np[idx]
+                pred = total_preds_np[idx]
+
+                ax[0].imshow(img.squeeze())
+                ax[0].set_title("Image")
+                ax[1].imshow(seg.squeeze())
+                ax[1].set_title("Ground Truth")
+                ax[2].imshow(pred.squeeze())
+                ax[2].set_title("Prediction")
+
+                plt.savefig(epoch_dir / f"pred{idx}")
+                plt.close()
+
+            pprint(metrics[-1])
     
     plt.title("Loss Per Epoch")
     plt.xlabel("Epoch")
-    # plt.ylabel("Dice Loss")
-    # plt.ylabel("Cross Entropy Loss")
     plt.ylabel("Focal Loss")
     plt.plot(train_losses)
     plt.plot(val_losses)
@@ -190,10 +236,45 @@ if __name__ == "__main__":
     segformer.config.image_size = image_size
     
     
+    # Run model
+    print(f"RUNNING WITH {len(train_data)} TRAIN SAMPLES AND {len(validation_data)} VALID SAMPLES")
+
+    loss = "focal"
+    dataset = "asoca"
+    num_epochs = 2
+    lr = 1e-03
+    batch_size = 8
+
+    out_dir = Path(f"segformer_training_results_{loss}_{dataset}")
+    out_dir.mkdir(exist_ok=True)
+
+    try:
+        segformer.load_state_dict(torch.load(out_dir / "segformer"))
+        print(f"Found model at {out_dir / 'segformer'}, resuming training")
+    except FileNotFoundError:
+        print(f"No model found at {out_dir / 'segformer'}, training from scratch")
+
+    start = time.time()
+
     train_segformer(
         segformer, 
         train_data, 
         validation_data,
-        25,
-        1e-03,
-        8)
+        num_epochs,
+        lr,
+        batch_size,
+        out_dir=out_dir
+        )
+
+    end = time.time()
+    total_seconds_elapsed = end - start
+    print(f"Training took {total_seconds_elapsed} seconds")
+    
+    # Save time elapsed
+    hours_elapsed = total_seconds_elapsed // 3600
+    minutes_elapsed = (total_seconds_elapsed % 3600) // 60
+    seconds_elapsed = total_seconds_elapsed % 60
+    with open(out_dir / "time_elapsed.txt", "w") as f:
+        f.write(f'Training took {total_seconds_elapsed} seconds\n')
+        f.write(f'Hours: {hours_elapsed}, Minutes: {minutes_elapsed}, Seconds: {seconds_elapsed}\n')
+    
