1dadbde0d2dba543de1af0885ddc19ee60a7a1b6
Henrik <henrrb@stud.ntnu.no>
2024-05-01 17:05:12 +0200

    Add temp file

---
+++ scripts/predict_test_segformer.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import json
+import seaborn as sns
+import torch.utils
+import torch.utils.data
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from tqdm import tqdm
+from pprint import pprint
+from pathlib import Path
+import nrrd
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from torchvision.transforms import Resize
+from transformers import SegformerForSemanticSegmentation, SegformerConfig
+from mis.models import UNet2D, UNet2DNonLocal
+from mis.datasets import ASOCADataset, BratsDataset
+from mis.settings import DEVICE, ASOCA_PATH
+from mis.loss import dice_loss, gdlv_loss, focal_loss
+
+if __name__ == "__main__":
+
+    model_dir = Path.cwd() / "unet2d_training_results_dice_asoca" / "model"
+    
+    config = SegformerConfig(num_channels=1, num_labels=1)
+    model = SegformerForSemanticSegmentation(config)
+    model.load_state_dict(torch.load(model_dir))
+
+    normal_dir = ASOCA_PATH / "Normal" / "Testset_Normal"
+    diseased_dir = ASOCA_PATH / "Diseased" / "Testset_Disease"
+
+    out_dir = Path.cwd() / "test_preds"
+    out_dir.mkdir(exist_ok=True)
+
+    size = 256
+    for i in range(10):
+        img, _ = nrrd.read(normal_dir / f"{i}.nrrd")
+        preds = np.zeros_like(img)
+        print(f"Predicting patient {i+1}...")
+        for slice_idx in tqdm(range(img.shape[-1])):
+            ctca = img[:, :, slice_idx][None, :, :]
+            ctca = ctca - ctca.min()
+            ctca = ctca / np.abs(ctca).max()
+            ctca = Resize((size, size))(torch.Tensor(ctca)).to(DEVICE)
+            preds_nt = model(ctca[None,:,:,:])[-1]
+            preds_nu = torch.where(preds_nt>=0.5, 1.0, 0.0)
+            preds_u = F.interpolate(preds_nu, scale_factor=2, mode="nearest")
+            preds[:,:,slice_idx] = preds_u.detach().cpu().numpy()
+
+        np.savez_compressed(out_dir/f"{i}")
+
+    for j in range(10, 20):
+        img, _ = nrrd.read(diseased_dir / f"{j}.nrrd")
+        preds = np.zeros_like(img)
+        print(f"Predicting patient {j+1}...")
+        for slice_idx in tqdm(range(img.shape[-1])):
+            ctca = img[:, :, slice_idx][None, :, :]
+            ctca = ctca - ctca.min()
+            ctca = ctca / np.abs(ctca).max()
+            ctca = Resize((size, size))(torch.Tensor(ctca)).to(DEVICE)
+            preds_nt = model(ctca[None,:,:,:])[-1]
+            preds_nu = torch.where(preds_nt>=0.5, 1.0, 0.0)
+            preds_u = F.interpolate(preds_nu, scale_factor=2, mode="nearest")
+            preds[:,:,slice_idx] = preds_u.detach().cpu().numpy()    
+
+        np.savez_compressed(out_dir/f"{j}")
